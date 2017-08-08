0dd44b0d4fe9497f621d13ce920d5fbbfafdcb53
Bill-Park <tkddn0916@gmail.com>
2017-08-08 23:05:54 +0900

    :sparkles: dust api fix

--- dust_api.py
+++ dust_api.py
@@ -1,21 +1,24 @@
+
 import requests
 import bill
 
 #서울, 부산, 대구, 인천, 광주, 대전, 울산, 경기, 강원, 충북, 충남, 전북, 전남, 경북, 경남, 제주, 세종
 
 def get_dust_api(sido):
-
+    print(sido)
     # 시군구별 실시간 평균정보(6)
-    url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst"
+    url = "http://openapi.airkorea.or.kr/openapi/services/rest/ArpltnInforInqireSvc/getCtprvnMesureSidoLIst?ServiceKey={}".format(bill.get_dust_key())
+
     metadata = {
-        "sidoName": sido,
+        "sidoName": "부산",
         "searchCondition": "HOUR",
         "pageNo": "1",
         "nuOfRows": "100",
-        "ServiceKey": bill.get_dust_key(),
         "_returnType": "json"
     }
+
     response = requests.get(url, params=metadata).json() # api run
+    print(response)
 
     for dust_data in response['list'] :
         if dust_data['cityName'] == '부산진구' :
