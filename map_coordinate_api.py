703557d8e6e13e63f41f26005bbbcd8547b61a91
Bill-Park <tkddn0916@gmail.com>
2017-08-20 11:39:48 +0900

    :tada: map coordinate modulization

--- map_coordinate_api.py
+++ map_coordinate_api.py
@@ -1,10 +1,12 @@
 import requests
-
-#https://57b1zt4gf3.execute-api.ap-northeast-2.amazonaws.com/dev?f1=부산광역시&f2=영도구&f3=동삼제1동"
+import get_key
 
 
 def get_weather_map_coordinate(f1="", f2="", f3=""):
-    base_url = "https://57b1zt4gf3.execute-api.ap-northeast-2.amazonaws.com/dev?"
+    base_url = "https://57b1zt4gf3.execute-api.ap-northeast-2.amazonaws.com/dev"
+    print(f1)
+    print(f2)
+    print(f3)
 
     arguments = {
         "f1": f1,
@@ -13,11 +15,27 @@ def get_weather_map_coordinate(f1="", f2="", f3=""):
     }
 
     response = requests.get(base_url, params=arguments).json()
-    #print(response['x'])
-    #print(response['y'])
 
     return response
 
 
-if __name__ == "__main__" :
+def get_map_address(longitude, latitude):
+    base_url = 'https://dapi.kakao.com/v2/local/geo/coord2regioncode.json'
+
+    arguments = {
+        "x": longitude,
+        "y": latitude
+    }
+
+    response = requests.post(base_url, params=arguments, headers={'Authorization': "KakaoAK " + get_key.get_map_key()}).json()
+
+    for map_address_list in response['documents']:
+        if map_address_list['region_type'] == 'H':
+            return map_address_list['region_1depth_name'], map_address_list['region_2depth_name'], map_address_list['region_3depth_name']
+
+    return None
+
+
+if __name__ == "__main__":
     print(get_weather_map_coordinate("서울특별시", "강남구"))
+    print(get_map_address(129.03, 35.15))   # 부산광역시 부산진구 가야2동
